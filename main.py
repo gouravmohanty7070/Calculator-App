ae13af247f77ae195edb1ae8a4f48d2e643e3556
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-08-04 05:59:01 +0000

    Functional Calculator

--- main.py
+++ main.py
@@ -15,3 +15,23 @@ def mul(n1, n2):
 #Divide
 def div(n1, n2):
   return n1 / n2
+
+#Dictionary
+application = {
+  '+': add,
+  '-': sub,
+  '*': mul,
+  '/': div
+}
+
+num1 = int(input("What's the first number? "))
+num2 = int(input("What's the second number? "))
+
+for i in application:
+  print(i)
+operation_symbol = input("Pick an symbol from the line above: ")
+
+function = application[operation_symbol]
+answer = function(num1,num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
