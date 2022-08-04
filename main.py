489ad5d37195a22bff35f815490f773efd669ce6
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-08-04 06:46:53 +0000

    Final Application

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Calculator
+from art import logo
+
 
 #ADD
 def add(n1, n2):
@@ -23,15 +25,33 @@ application = {
   '*': mul,
   '/': div
 }
+def calculator():
+  print(logo)
+  num1 = float(input("What's the first number? "))
+  
+  for i in application:
+    print(i)
+  operation_symbol = input("Pick an symbol from the line above: ")
+  
+  flag = True
+  
+  answer = num1 
+  
+  while flag:
+    num2 = float(input("What's the next number? "))
+    
+    function = application[operation_symbol]
+    answer = function(num1,num2)
+  
+  
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    repeat = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
+  
+    if repeat == "y":
+      operation_symbol = input("Pick an operation: ")
+      num1 = answer
+    else:
+      flag = False
+      calculator()
 
-num1 = int(input("What's the first number? "))
-num2 = int(input("What's the second number? "))
-
-for i in application:
-  print(i)
-operation_symbol = input("Pick an symbol from the line above: ")
-
-function = application[operation_symbol]
-answer = function(num1,num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+calculator()
