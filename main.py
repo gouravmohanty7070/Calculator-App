7f9355333bc32aa8d021173d71d8696f84480275
Gourav Mohanty <gouravmohanty2001@gmail.com>
2022-08-04 05:47:38 +0000

    Features of application added.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #Calculator
+
+#ADD
+def add(n1, n2):
+  return n1 + n2
+
+#SUBSTRACT
+def sub(n1, n2):
+  return n1 - n2
+
+#MULTIPLY
+def mul(n1, n2):
+  return n1 * n2
+
+#Divide
+def div(n1, n2):
+  return n1 / n2
